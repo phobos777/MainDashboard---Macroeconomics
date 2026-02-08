601778f1d11b018a777d9359b887e7b404b9d567
phobos777 <jasonforish@hotmail.com>
2026-02-08 16:14:15 -0500

    Update MainDashboard61.py to remove specific variables and lines

--- MainDashboard61.py
+++ MainDashboard61.py
@@ -13,14 +13,6 @@ risk_signals = ['Risk On', 'Risk Off', 'Risk On', 'Risk Off']
 usdt_dominance = [0.45, 0.47, 0.44, 0.49]
 fear_greed_index = [50, 60, 40, 35]
 golden_cross = [10000, 12000, 11000, 11500]
-ema_cross_analysis = [0, 1, 1, 0]
-btc_funding_rates = [0.01, -0.005, 0.002, 0.003]
-btc_etf_inflows = [100, 200, 150, 300]
-cbbc_components = [0.5, 0.7, 0.65, 0.6]
-
-# Adding headers
-headers = ['Date', 'Risk Signal', 'USDT Dominance', 'Fear & Greed Index', 'Golden Cross', 'EMA Cross', 'BTC Funding Rates', 'BTC ETF Inflows', 'CBBI']
-worksheet.append(headers)
 
 # Adding sample data
 for i in range(len(risk_signals)):
@@ -29,11 +21,7 @@ for i in range(len(risk_signals)):
         risk_signals[i], 
         usdt_dominance[i], 
         fear_greed_index[i], 
-        golden_cross[i], 
-        ema_cross_analysis[i], 
-        btc_funding_rates[i], 
-        btc_etf_inflows[i], 
-        cbbc_components[i]
+        golden_cross[i]
     ])
 
 # Save the workbook
